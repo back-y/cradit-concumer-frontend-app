a2190d1e12e3d5326434d109025540b4aa4cedf0
Anwar Endris <anwarendo@gmail.com>
2024-01-17 15:45:15 +0300

    Update success and failure pages

--- src/pages/clients/shopindividual/failure.jsx
+++ src/pages/clients/shopindividual/failure.jsx
@@ -3,15 +3,26 @@
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import { Typography } from '@mui/material'
+import { useRouter } from 'next/router'
 
 const shop = () => {
+  const router = useRouter();
   
+  setTimeout(() => {
+    router.push('/clients/shopindividual')
+  }, 5000)
   return (
     <section>
       <Card>
         <CardContent>
-          <Typography>
-            Your order has <b>failed. </b> Please try again. 
+          <Typography style={{ marginLeft: '10px', fontSize: '20px' }} >
+            Your order ... 
+          </Typography>
+          <Typography style={{ marginLeft: '10px', fontWeight: 'bold', color: 'red', fontSize: '20px' }} >
+            has failed 
+          </Typography>
+          <Typography style={{ marginLeft: '10px', fontSize: '20px' }} >
+            Please try again 
           </Typography>
         </CardContent>
         
--- src/pages/clients/shopindividual/success.jsx
+++ src/pages/clients/shopindividual/success.jsx
@@ -3,15 +3,22 @@
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import { Typography } from '@mui/material'
-
+import { useRouter } from 'next/router'
 const shop = () => {
-  
+  const router = useRouter()
+
+  setTimeout(() => {
+    router.push('/clients/shopindividual')
+  }, 5000)
   return (
     <section>
       <Card>
         <CardContent>
-          <Typography>
-            Your order has been fulfilled <b>successfully!</b> 
+        <Typography style={{ marginLeft: '10px', fontSize: '20px' }} >
+            Your order has been fulfilled
+          </Typography>
+          <Typography style={{ marginLeft: '10px', fontWeight: 'bold', color: 'green', fontSize: '20px' }} >
+            successfully!
           </Typography>
         </CardContent>
         
