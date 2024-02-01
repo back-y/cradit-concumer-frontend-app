e55592f7be41c85ea6422c6226bf2886b42cc999
Estifanos Hayilye <epowerbase@gmail.com>
2024-01-31 22:19:10 -0800

    update graph data and credit home page card data

--- src/pages/credit/BarsGraph.js
+++ src/pages/credit/BarsGraph.js
@@ -1,33 +1,49 @@
-// BarGraph.js
 
 import React from 'react';
 import Card from '@mui/material/Card';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-    { name: 'Total Credit Given', Birr: 0 },
-    { name: 'Total Credit Paid', Birr: 0 },
-    { name: 'Total Credit Unpaid', Birr: 0 },
-    { name: 'Total Unpaid Credit With Interest', Birr: 0 },
-];
-
-
-
-
-
-
-
-
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const BarGraph = () => {
+
+    const [salesData, setSalesData] = useState([])
+
+    useEffect(() => {
+        const getter = async () => {
+            const url = process.env.NEXT_PUBLIC_API_URL + 'credit/totalCreditInfo'
+            const resp = await axios.get(url)
+            setSalesData(resp.data)
+
+            //   const fine = resp.data
+            console.log('resp ,data', resp.data)
+        }
+
+        getter()
+    }, [])
+
+    const colorScale = (value) => {
+        if (value < 5000) {
+            return '#FF5733'; // Set color for values less than 5000
+        } else if (value >= 5000 && value < 10000) {
+            return '#FFC300'; // Set color for values between 5000 and 10000
+        } else {
+            return '#36D7B7'; // Set color for values greater than or equal to 10000
+        }
+    };
+
+    const transformedData = salesData.map(item => ({
+        name: item.title,
+        ETB: parseFloat(item.stats.replace(' ETB', ''))
+    }));
+
     return (
         <Card>
             <ResponsiveContainer width="100%" height={300}>
                 <BarChart
                     width={300}
                     height={300}
-                    data={data}
+                    data={transformedData}
                     margin={{
                         top: 20,
                         right: 30,
@@ -37,10 +53,11 @@ const BarGraph = () => {
                 >
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" style={{ fontSize: 14, color: ' #500000' }} />
-                    <YAxis />
+                    <YAxis tickFormatter={value => value.toLocaleString()} />
+
                     <Tooltip />
                     <Legend />
-                    <Bar dataKey="Birr" fill="#8884d8" />
+                    <Bar tickFormatter={value => value.toLocaleString()} dataKey="ETB" fill="#8884d8" scale={colorScale} />
                 </BarChart>
             </ResponsiveContainer>
         </Card>
